fee7b003c20eb274beedb65b34d54f89237ae0c4
dhairya-pro <shahdhairya069@gmail.com>
2025-11-28 21:16:42 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    strictPort: true,
+  },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
